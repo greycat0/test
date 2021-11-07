74cd95131bfc3eb82bf44a8077cdae78b38270d0
Amir <amir@musaev.online>
2021-11-07 22:39:46 +0100

    fix(app): deploy

---
+++ _next/static/GEO_9jTF9M97x1tJxWMSn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/922-38075578dbdf90b9.js","static/chunks/pages/index-739afca39d7226ec.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
